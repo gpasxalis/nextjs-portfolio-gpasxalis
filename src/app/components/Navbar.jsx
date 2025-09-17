53e4f619c5f8c799745f6e253d398e7c9fa8fb78
gpasxalis <gpasxalis94@gmail.com>
2025-09-17 14:51:59 +0300

    Fixes

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -146,10 +146,10 @@ export const Navbar = () => {
                         exit={{ opacity: 0, y: -20 }}
                         transition={{ duration: 0.3 }}
                         className={`min-[825px]:hidden fixed w-full top-[64px] z-40 ${
-                            isScrolled ? 'bg-black/30 backdrop-blur-lg ' : ''
+                            isScrolled ? 'bg-black/30 backdrop-blur-lg' : ''
                         }`}
                     >
-                        <div className="bg-gp-blue-bg/80 backdrop-blur-3xl shadow-gp-accent/20 shadow-lg border border-gp-accent/20 rounded-2xl mx-4 mt-2 p-6">
+                        <div className="bg-gp-blue-bg/95 backdrop-blur-3xl shadow-gp-accent/20 shadow-lg border border-gp-accent/20 rounded-2xl mx-4 mt-2 p-6">
                             <div className="flex flex-col gap-4">
                                 {navLinks.map((link, index) => (
                                     <motion.a
